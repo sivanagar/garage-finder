dc995f34b5d7adb86c3fa9dc8ff078368367acde
joseduardo15062005 <joseduardo15062005@gmail.com>
2022-01-05 10:27:47 -0800

    add autocomplete input

---
+++ client/src/components/SearchAutoComplete/index.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import PlacesAutoComplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+
+const SearchAutoComplete = () => {
+  const [address, setAddress] = useState("");
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lng: null,
+  });
+
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setAddress(value);
+    setCoordinates(latLng);
+    console.log("Resutls", results);
+  };
+
+  return (
+    <>
+      <PlacesAutoComplete
+        value={address}
+        onChange={setAddress}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <p>Latitude: {coordinates.lat}</p>
+            <p>Longitude: {coordinates.lng}</p>
+
+            <input {...getInputProps({ placeholder: "Type address" })} />
+
+            <div>
+              {loading ? <div>...loading</div> : null}
+
+              {suggestions.map((suggestion) => {
+                const style = {
+                  backgroundColor: suggestion.active ? "#41b6e6" : "#fff",
+                };
+                return (
+                  <div
+                    key={suggestion.placeId}
+                    {...getSuggestionItemProps(suggestion, { style })}
+                  >
+                    {suggestion.description}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutoComplete>
+    </>
+  );
+};
+
+export default SearchAutoComplete;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,6 @@
+import React from "react";
+import SearchAutoComplete from "../components/SearchAutoComplete";
+
+const Search = () => <SearchAutoComplete />;
+
+export default Search;
