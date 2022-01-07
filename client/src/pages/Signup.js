22c2dbd963d04b1e647e9aaa546e755d08289b80
joseduardo15062005 <joseduardo15062005@gmail.com>
2022-01-06 18:32:18 -0800

    add signup form

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
-
-import Auth from '../utils/auth';
+import { useMutation } from "@apollo/client";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  FormControl,
+  Heading,
+  Image,
+  Input,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
 
 const Signup = () => {
   const [formState, setFormState] = useState({
-    username: '',
-    email: '',
-    password: '',
+    username: "",
+    email: "",
+    password: "",
   });
   const [addUser, { error }] = useMutation(ADD_USER);
 
@@ -38,49 +47,67 @@ const Signup = () => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-md-6">
-        <div className="card">
-          <h4 className="card-header">Sign Up</h4>
-          <div className="card-body">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="Your username"
-                name="username"
-                type="username"
-                id="username"
-                value={formState.username}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                id="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className="btn d-block w-100" type="submit">
-                Submit
-              </button>
-            </form>
+    <Flex w={[300, 400]} direction="column" alignItems="center" mt="20">
+      <Box
+        borderWidth="1px"
+        borderColor="primary"
+        borderRadius="lg"
+        boxShadow="lg"
+        w="100%"
+        p={[4, 10]}
+      >
+        <Center>
+          <Image
+            boxSize="100px"
+            src="https://www.gstatic.com/images/branding/product/1x/keep_48dp.png"
+          />
+        </Center>
+        <Flex mb="6" justify="center">
+          <Heading>Signup</Heading>
+        </Flex>
+        <form onSubmit={handleFormSubmit} style={{ width: "100%" }}>
+          <FormControl mb="6">
+            <Input
+              placeholder="Your username"
+              name="username"
+              type="username"
+              id="username"
+              value={formState.username}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl mb="6">
+            <Input
+              placeholder="Your email"
+              name="email"
+              type="email"
+              id="email"
+              value={formState.email}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-            {error && <div>Signup failed</div>}
-          </div>
-        </div>
-      </div>
-    </main>
+          <FormControl mb="6">
+            <Input
+              placeholder="Your password"
+              name="password"
+              type="password"
+              id="password"
+              value={formState.password}
+              onChange={handleChange}
+            />
+          </FormControl>
+
+          <Center>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Center>
+        </form>
+
+        {error && <div>Signup failed</div>}
+      </Box>
+    </Flex>
   );
 };
 
