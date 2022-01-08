07fb7e103b2785c5c7c932d3b8fe5312d4e29d54
joseduardo15062005 <joseduardo15062005@gmail.com>
2022-01-07 18:02:50 -0800

    add home page functionality

---
+++ client/src/components/ResultHome/index.js
@@ -0,0 +1,70 @@
+import { StarIcon } from "@chakra-ui/icons";
+import { Badge, Box } from "@chakra-ui/react";
+
+const ResultHome = ({ listing }) => {
+  const {
+    rate,
+    address,
+    city,
+    state,
+    zip,
+    location,
+    accessType,
+    climateControl,
+    width,
+    height,
+    depth,
+    type,
+  } = listing;
+  return (
+    <Box minW={[340, 240]} borderWidth="1px" borderRadius="lg" m="1">
+      <Box p="6">
+        <Box display="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="2" colorScheme="teal">
+            New
+          </Badge>
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+          >
+            {`${height}' x ${width}' x ${depth}'`}
+          </Box>
+        </Box>
+
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          isTruncated
+        >
+          {type}
+        </Box>
+
+        <Box>
+          {rate}
+          <Box as="span" color="gray.600" fontSize="sm">
+            / m
+          </Box>
+        </Box>
+
+        <Box display="flex" mt="2" alignItems="center">
+          {Array(5)
+            .fill("")
+            .map((_, i) => (
+              <StarIcon key={i} color={i < 20 ? "primary" : "gray.300"} />
+            ))}
+          <Box as="span" ml="2" color="gray.600" fontSize="sm">
+            20 reviews
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default ResultHome;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,17 @@
 import { useQuery } from "@apollo/client";
-import { Box, Button, Center, Flex, Skeleton, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Heading,
+  Skeleton,
+  Stack,
+} from "@chakra-ui/react";
+import _ from "lodash";
 import { useState } from "react";
 import { Link as ReactLink, useHistory } from "react-router-dom";
+import ResultHome from "../components/ResultHome";
 import SearchAutoComplete from "../components/SearchAutoComplete";
 import Auth from "../utils/auth";
 import { QUERY_ALL_LISTINGS, QUERY_ME } from "../utils/queries";
@@ -40,9 +50,11 @@ const Home = () => {
       </Skeleton>
     );
   }
+  let randomListings = [];
 
   if (!listingsLoading) {
-    console.log("Listings: ", listingsData.listings);
+    randomListings = _.sampleSize(listingsData.listings, 3);
+    console.log(randomListings);
   }
 
   function handleClickSearch() {
@@ -56,7 +68,24 @@ const Home = () => {
 
   return (
     <>
+      <Flex w="100%" mt="20" justify={["center", "flex-start"]}>
+        <Heading>Get your space now </Heading>
+      </Flex>
+      <Flex
+        w="100%"
+        mt="4"
+        direction={["column", "row"]}
+        justify={["center", "flex-start"]}
+        alignItems={["center", "flex-start"]}
+      >
+        {randomListings.map((listing) => (
+          <ResultHome listing={listing} key={listing._id} />
+        ))}
+      </Flex>
       <Box w="100%" mt="20">
+        <Center mb="2">
+          <Heading>Find a space</Heading>
+        </Center>
         <SearchAutoComplete setResult={setAddressResult} />
         <Center mt="4">
           <Button onClick={handleClickSearch} variant="primary" size="lg">
@@ -64,13 +93,63 @@ const Home = () => {
           </Button>
         </Center>
       </Box>
-      <Flex direction="column" align="center" justify="center" mt="40">
-        <Button as={ReactLink} to="search" variant="primary" m="4" size="lg">
-          I need to rent space
-        </Button>
-        <Button as={ReactLink} to="/rent" variant="primary" m="4" size="lg">
-          I have space to rent
-        </Button>
+      <Flex
+        w="100%"
+        my="6"
+        direction={["column", "row"]}
+        justify={["center"]}
+        alignItems={["center", "flex-start"]}
+      >
+        <Box
+          minW={[300, "50%"]}
+          minH={[300]}
+          borderWidth="1px"
+          borderRadius="lg"
+          m="1"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="space-between"
+          bgGradient="url('https://sanfrancisco.cbslocal.com/wp-content/uploads/sites/15116056/2015/03/cnn-single-mom-garage.jpg?w=701')"
+          bgSize="cover"
+        >
+          <Flex w="100%" justify="flex-start" p="4">
+            <Heading color="white">You need aditional space</Heading>
+          </Flex>
+          <Flex w="100%" justify="flex-end">
+            <Button
+              as={ReactLink}
+              to="search"
+              variant="primary"
+              m="4"
+              size="lg"
+            >
+              I need to rent space
+            </Button>
+          </Flex>
+        </Box>
+        <Box
+          minW={[300, "50%"]}
+          minH={[300]}
+          borderWidth="1px"
+          borderRadius="lg"
+          m="1"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="space-between"
+          bgGradient="url('https://cdn.motor1.com/images/mgl/b0ql3/s1/4x3/gearhead-home-offers-6-car-garage-with-bonus-room-for-under-400k.webp')"
+          bgSize="cover"
+        >
+          <Flex w="100%" justify="flex-start" p="4">
+            <Heading color="white">I Have Extra Space at home</Heading>
+          </Flex>
+          <Flex w="100%" justify="flex-end">
+            <Button as={ReactLink} to="/rent" variant="primary" m="4" size="lg">
+              I have space to rent
+            </Button>
+          </Flex>
+        </Box>
       </Flex>
     </>
   );
